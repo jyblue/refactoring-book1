51b57f78fd6bc611b549502023265546ecd7c484
jyblue <jyblue.kim@gmail.com>
2022-07-04 23:17:07 +0900

    Add Chapter06 client code

--- chapter06/Program.cs
+++ chapter06/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace chapter06
+﻿using chapter06.before;
+using System;
+
+namespace chapter06
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Book refactoringBook = new Book(
+                "Refactoring",
+                "ISBN1234",
+                "$10.00",
+                "Martin Fowler",
+                "m@abc.com");
+
+            Book javaBook = new Book(
+                "Java",
+                "ISBN1313",
+                "$12.00",
+                "Goooosling",
+                "g@bbb.com");
+
+            Console.WriteLine("refactoring:");
+            Console.WriteLine(refactoringBook.ToString());
+
+            Console.WriteLine("java:");
+            Console.WriteLine(javaBook.ToString());
         }
     }
 }
