839d4c8979e2b839751d279dff35634d61773761
jyblue <jyblue.kim@gmail.com>
2022-07-04 22:17:43 +0900

    리팩토링 전

---
+++ chapter05/Program.cs
@@ -0,0 +1,13 @@
+﻿using chapter05.before;
+
+namespace chapter05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banner banner = new Banner("Extract Method");
+            banner.print(3);
+        }
+    }
+}
---
+++ chapter05/before/Banner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace chapter05.before
+{
+    public class Banner
+    {
+        private readonly string _content;
+
+        public Banner(string content)
+        {
+            this._content = content;
+        }
+
+        public void print(int times)
+        {
+            // 테두리 출력
+            Console.Write("+");
+            for (int i = 0; i < _content.Length; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+
+            // 내용 출력
+            for (int i = 0; i < times; i++)
+            {
+                Console.WriteLine("|" + _content + "|");
+            }
+
+            // 테두리 출력
+            Console.Write("+");
+            for (int i = 0; i < _content.Length; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+        }
+    }
+}
