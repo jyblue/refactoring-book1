c988b842d4e7530774bfa3f1014e469c65240ece
jyblue <jyblue.kim@gmail.com>
2022-07-05 22:19:20 +0900

    Chapter07_before

---
+++ chapter07/Program.cs
@@ -0,0 +1,30 @@
+﻿using chapter07.before;
+using System;
+
+namespace chapter07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item book = new Item(
+                Item.TYPECODE_BOOK,
+                "History",
+                4800);
+
+            Item dvd = new Item(
+                Item.TYPECODE_DVD,
+                "New York",
+                3000);
+
+            Item soft = new Item(
+                Item.TYPECODE_SOFTWARE,
+                "Turing",
+                3200);
+
+            Console.WriteLine(book.ToString());
+            Console.WriteLine(dvd.ToString());
+            Console.WriteLine(soft.ToString());
+        }
+    }
+}
---
+++ chapter07/before/Item.cs
@@ -0,0 +1,34 @@
+﻿namespace chapter07.before
+{
+    public class Item
+    {
+        public static readonly int TYPECODE_BOOK = 0;
+        public static readonly int TYPECODE_DVD = 1;
+        public static readonly int TYPECODE_SOFTWARE = 2;
+
+        private readonly int typecode;
+        private readonly string title;
+        private readonly int price;
+
+        public Item(
+            int typecode,
+            string title,
+            int price)
+        {
+            this.typecode = typecode;
+            this.title = title;
+            this.price = price;
+        }
+
+        public int GetTypeCode() { return typecode; }
+        public string GetTitle() { return title; }
+        public int GetPrice() { return price; }
+        public override string ToString()
+        {
+            return "[" 
+                + GetTypeCode() + ","
+                + GetTitle() + ","
+                + GetPrice() + "]";
+        }
+    }
+}
