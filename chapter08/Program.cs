bfae8de0d76ce0dd4d41a41b6792316e257b67d2
jyblue <jyblue.kim@gmail.com>
2022-07-12 23:04:10 +0900

    Add chapter08 before

---
+++ chapter08/Program.cs
@@ -0,0 +1,24 @@
+﻿using chapter08.before;
+
+namespace chapter08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape line = new Shape(Shape.TYPECODE_LINE, 0, 0, 100, 200);
+            Shape rectangle = new Shape(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40);
+            Shape oval = new Shape(Shape.TYPECODE_OVAL, 100, 200, 300, 400);
+
+            Shape[] shape =
+            {
+                line, rectangle, oval
+            };
+
+            foreach(Shape s in shape)
+            {
+                s.Draw();
+            }
+        }
+    }
+}
